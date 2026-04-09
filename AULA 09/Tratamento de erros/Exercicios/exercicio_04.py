0a3bc6644d5aecc0c66272bc019611e7711db5a6
Henrique Dos Santos Miranda <fegemeos4@gmail.com>
2026-04-09 20:10:31 -0300

    Término dos exercicios da aula passada sobre Try, except e finally e a junção com loop

--- AULA 09/Tratamento de erros/Exercicios/exercicio_04.py
+++ AULA 09/Tratamento de erros/Exercicios/exercicio_04.py
@@ -1,8 +1,8 @@
-cores = ["Azul", "Verde", "Amarelo"]
-
-opcao = int(input("Escolha um numero de 0 a 2 e direi sua cor: "))
-# cores = opcao
 try:
+    cores = ["Azul", "Verde", "Amarelo"]
+
+    opcao = int(input("Escolha um numero de 0 a 2 e direi sua cor: "))
+
     print(f"Sua cor é: {cores[opcao]}")
 except IndexError:
     print("Posição inexistente!")
