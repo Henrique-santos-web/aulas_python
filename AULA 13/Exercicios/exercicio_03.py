d836930fc8cc6a911ed1aaa44b443cc02d5024a3
Henrique Dos Santos Miranda <fegemeos4@gmail.com>
2026-04-23 21:33:23 -0300

    contador de vogais

--- AULA 13/Exercicios/exercicio_03.py
+++ AULA 13/Exercicios/exercicio_03.py
@@ -1,8 +1,17 @@
 def contar_vogais(texto):
+    texto = texto.lower()
     total = 0
-    texto = input("Digite uma palavra: ").lower()
+    for letra in texto:
+        if letra in "aeiou":
+            total += 1
+    return total  # A função devolve apenas o NÚMERO
 
-    for vogais in len(texto):
-        if vogais == "a, e, i, o, u":
-            
-        vogais = texto
+
+# 1. Primeiro pegamos o texto do usuário
+palavra = input("Digite uma palavra: ")
+
+# 2. Chamamos a função passando esse texto e guardamos o retorno
+quantidade = contar_vogais(palavra)
+
+# 3. Exibimos o resultado usando as variáveis que temos aqui fora
+print(f"A palavra '{palavra}' tem {quantidade} vogais.")
