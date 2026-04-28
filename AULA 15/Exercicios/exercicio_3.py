7d4d0439bbe96efdc506da70659dd83f8e3c250b
Henrique Miranda <fegemeos4@gmail.com>
2026-04-27 21:38:30 -0300

    material de estudo

---
+++ AULA 15/Exercicios/exercicio_3.py
@@ -0,0 +1,15 @@
+class Cachorro:
+    def __init__(self, nome, raca):
+    #* metodo __init__ é automatizar os atributos ao objeto (pesquisar melhor isso)
+        self.nome = nome
+        self.raca = raca 
+
+        #* self.nome/raca = atributo
+        #* o que vem após o = é o que ficara dentro dos parametros da def __init__
+
+
+cachorro1 = Cachorro("Rex", "Caramelo")
+print(f"{cachorro1.nome}")
+
+#* metodo = ação
+#* atributo = o que ele tem
