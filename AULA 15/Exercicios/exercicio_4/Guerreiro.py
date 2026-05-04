aa4505a01766cd90e9ca3cd27e768c6bc8f6aeeb
Henrique Miranda <fegemeos4@gmail.com>
2026-05-04 19:24:15 -0300

    Material de estudo - POO

--- AULA 15/Exercicios/exercicio_4/Guerreiro.py
+++ AULA 15/Exercicios/exercicio_4/Guerreiro.py
@@ -1,6 +1,11 @@
-class Guerreito:
+class Guerreiro:
     def __init__(self, nome):
         self.nome = nome
         self.vida = 100
 
-    def dano()
+    def sofrer_dano(self, dano):
+        self.vida -= dano
+        return f"Você sofreu {dano} de dano. Status de HP: {self.vida}"
+
+guerreiro = Guerreiro("Pedro")
+print(guerreiro.sofrer_dano(20))
