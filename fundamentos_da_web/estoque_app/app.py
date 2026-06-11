e14b96c626beb4a7490cf5e03a35be3b491b4271
Henrique Miranda <fegemeos4@gmail.com>
2026-06-10 21:36:33 -0300

    feat: implementando Flask com SQLAlchemy

---
+++ fundamentos_da_web/estoque_app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from extensions import db 
+from models import Produto
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///estoque.db" # *Caminho para o banco de dados
+app.config['SQLALCHEMY_TRACK_'] = False # *Essa config é pra não mostrar toda hora no terminal as transações no banco de dados
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+    print("Banco de dados e tabela criadas com sucesso!")
+
+@app.route("/api/produtos", methods="POST")
+def cadastrar_produto():
+    dados = request.get_json()
+    # *o request é uma requisição (dados enviado pelo usuario (neste caso aqui é enviado por nos pelo app postman) que está salvo dentro do Flask)
+    # *chamamos essa requisição usando o command = request 
+    # *e após o ponto é o formato que queremos esses dados (neste caso é em json) 
+
+    novo_produto = Produto(nome=dados["nome"], preco=dados["preco"])
+
+    db.session.add(novo_produto)
+    db.session.commit()
+
+    return jsonify(novo_produto.to_dict()), 201
+
+
+@app.route("/api/produtos", methods="GET")
+def 
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ fundamentos_da_web/estoque_app/models.py
@@ -0,0 +1,11 @@
+from extensions import db
+
+class Produto(db.Model):
+    __tablename__ = "produtos"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(80), nullable=False)
+    preco = db.Column(db.Float(120), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {"id" : self.id, "nome" : self.nome, "preco" : self.preco}
