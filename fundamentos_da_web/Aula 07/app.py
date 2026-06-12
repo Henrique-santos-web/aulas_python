032fa8a91dedf9b90837a095aa90a6ef8ab73277
Henrique Miranda <fegemeos4@gmail.com>
2026-06-11 21:20:50 -0300

    feat: deposi escrevo

--- fundamentos_da_web/Aula 07/app.py
+++ fundamentos_da_web/Aula 07/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from extensions import db 
+from extensions import db
+from sqlalchemy.exc import IntegrityError
 from models import Usuario
 
 app = Flask(__name__)
@@ -13,24 +14,72 @@ with app.app_context():
     print("Banco de dados e tabela criadas com sucesso!")
 
 
-@app.route("/api/usuarios", methods="POST")
+@app.route("/api/usuarios", methods=["POST"])
 def criar_usuario():
     dados = request.get_json()
+
+    if 
 
     novo_usuario = Usuario(nome=dados["nome"], email=dados["email"])
 
     db.session.add(novo_usuario)
     db.session.commit()
 
+
     return jsonify(novo_usuario.to_dict()), 201
 
-@app.route("/api/usuarios", methods="GET")
+@app.route("/api/usuarios", methods=["GET"])
 def listar_usuarios():
     usuarios = Usuario.query.all()
 
-    lista_json = [usuario.to_dict() for usuario in usuarios] # * transforme usuario em dicionario para(for) cada usuario em(in) usuarios
+    lista_json = [usuario.to_dict() for usuario in usuarios] # * transforme usuarios em dicionario para(for) cada usuario em(in) usuarios
 
     return jsonify(lista_json), 200
+
+
+@app.route("/api/usuarios/<int:id>", methods=["PUT"])
+def atualizar_usuario(id):
+    user = Usuario.query.get_or_404(id)
+    dados = request.get_json()
+
+    user.nome = dados["nome"]
+    user.email = dados["email"]
+
+    db.session.commit()
+
+    return jsonify({"mensagem" : "Atualizar"}), 200
+
+@app.route("/api/usuarios", methods=["PATCH"])
+def atualizar_parcial(id):
+    user = Usuario.query.get_or_404(id)
+    dados = request.get_json()
+
+    if "nome" in dados:
+        user.nome = dados["nome"]
+    if "nome" in dados:
+        user.email = dados["email"]
+
+    db.session.commit()
+    return jsonify({"mensagem" : "Registro Ajustado"}), 200
+
+
+@app.route("/api/usuarios", methods=["DELETE"])
+def deletar_usuario(id):
+    user = Usuario.query.get_or_404(id)
+    dados = request.get_json() # * no delete não é usado esse comando, pois esse comando serve pra pegar um corpo (comando em dicionario do postman no body -> row)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"mensagem" : "Registro deletado"}), 200
+
+@app.errorhandler(404)
+def rota_nao_encontrada(erro):
+    return jsonify({
+        erro : "RONA_NAO_ENCONTRADA",
+        "mensagem" : "O endpoint solicitado não existe"
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
